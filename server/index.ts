e406418d49aff386821876f09bee61907beccd85
Runecoins <runecoinsot@gmail.com>
2026-02-26 15:37:30 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -60,9 +60,12 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  await registerRoutes(httpServer, app);
+
+if (process.env.NODE_ENV !== "production") {
   const { seedDatabase } = await import("./seed");
   await seedDatabase();
-  await registerRoutes(httpServer, app);
+}
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -91,15 +94,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
+  const port = parseInt(process.env.PORT || "3000", 10);
+
+httpServer.listen(port, "0.0.0.0", () => {
+  log(`serving on port ${port}`);
+});
   );
 })();
